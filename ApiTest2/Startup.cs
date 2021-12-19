a7d6ee7c0f3ba2d7a0f2119b90e0e4d316d8c436
abdullah <abdullahtopal0@gmail.com>
2021-12-19 03:13:13 +0300

    commit

--- ApiTest2/Startup.cs
+++ ApiTest2/Startup.cs
@@ -29,8 +29,8 @@ namespace ApiTest2
         {
             //AOP : 
             services.AddControllers();
-            services.AddSingleton<IProductService, ProductManager>();
-            services.AddSingleton<IProductDal, EfProductDal>();
+            //services.AddSingleton<IProductService, ProductManager>();
+            //services.AddSingleton<IProductDal, EfProductDal>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
